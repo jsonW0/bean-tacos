6c5b8f81e744104c606b066be5e02afe9163ae7f
Jason Wang <43554820+jsonW0@users.noreply.github.com>
2024-11-22 23:32:31 -0500

    tacos working

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-# clear; python -m runner.synthesize --topology tests/biangle.csv --collective all_gather --synthesizer naive
-# clear; python visualize_collective.py --filename results/t=tests-biangle_c=all_gather_s=naive/result.csv
+clear; python -m runner.synthesize --topology tests/biangle.csv --collective all_gather --synthesizer tacos
+python visualize_collective.py --filename results/t=tests-biangle_c=all_gather_s=tacos/result.csv
 
-# clear; python -m runner.synthesize --topology tests/triangle.csv --collective all_gather --synthesizer naive
-# python visualize_collective.py --filename results/t=tests-triangle_c=all_gather_s=naive/result.csv
+clear; python -m runner.synthesize --topology tests/triangle.csv --collective all_gather --synthesizer tacos
+python visualize_collective.py --filename results/t=tests-triangle_c=all_gather_s=tacos/result.csv
 
-# clear; python -m runner.synthesize --topology tests/quadangle.csv --collective all_gather --synthesizer naive
-# python visualize_collective.py --filename results/t=tests-quadangle_c=all_gather_s=naive/result.csv
+clear; python -m runner.synthesize --topology tests/quadangle.csv --collective all_gather --synthesizer tacos
+python visualize_collective.py --filename results/t=tests-quadangle_c=all_gather_s=tacos/result.csv
 
-# clear; python -m runner.synthesize --topology tests/bidirectional_ring.csv --collective all_gather --synthesizer naive
-# python visualize_collective.py --filename results/t=tests-bidirectional_ring_c=all_gather_s=naive/result.csv
+clear; python -m runner.synthesize --topology tests/bidirectional_ring.csv --collective all_gather --synthesizer tacos
+python visualize_collective.py --filename results/t=tests-bidirectional_ring_c=all_gather_s=tacos/result.csv
 
-clear; python -m runner.synthesize --topology tests/bidirectional_ring_slow.csv --collective all_gather --synthesizer naive
-python visualize_collective.py --filename results/t=tests-bidirectional_ring_slow_c=all_gather_s=naive/result.csv
+clear; python -m runner.synthesize --topology tests/bidirectional_ring_slow.csv --collective all_gather --synthesizer tacos
+python visualize_collective.py --filename results/t=tests-bidirectional_ring_slow_c=all_gather_s=tacos/result.csv
 
-# clear; python -m runner.synthesize --topology tests/heterogeneous.csv --collective all_gather --synthesizer naive
-# python visualize_collective.py --filename results/t=tests-heterogeneous_c=all_gather_s=naive/result.csv
+clear; python -m runner.synthesize --topology tests/heterogeneous.csv --collective all_gather --synthesizer tacos
+python visualize_collective.py --filename results/t=tests-heterogeneous_c=all_gather_s=tacos/result.csv
 
-# clear; python -m runner.synthesize --topology grid --collective all_gather --synthesizer naive --verbose
-# python visualize_collective.py --filename results/t=grid_c=all_gather_s=naive/result.csv
+clear; python -m runner.synthesize --topology grid --collective all_gather --synthesizer tacos --verbose
+python visualize_collective.py --filename results/t=grid_c=all_gather_s=tacos/result.csv
--- synthesizer/naive_synthesizer.py
+++ synthesizer/naive_synthesizer.py
@@ -2,7 +2,7 @@ import csv
 import random
 from collections import defaultdict
 from helper.typing import *
-from helper.time_expanded_network import TimeExpandedNetwork
+from helper.event_queue import EventQueue
 from topology.topology import Topology
 from collective.collective import Collective
 
@@ -16,23 +16,82 @@ class NaiveSynthesizer:
         self.edges = self.topology.G.edges
         self.chunks = self.collective.chunks
 
-        self.TEN = TimeExpandedNetwork(topology=self.topology, collective=self.collective)
-
         if discretize:
-            self.TEN.discretize()
+            self.discretize()
+
+        self.current_time = 0
+        self.event_history: List[Event] = []
+        self.event_queue = EventQueue()
+
+        self.link_busy_until = {edge:0 for edge in self.topology.G.edges}
+        self.chunk_arrival_at_node = {node:defaultdict(lambda:float('inf')) for node in self.topology.G.nodes}
+        for chunk, node in self.collective.precondition:
+            self.chunk_arrival_at_node[node][chunk] = 0
+    
+    def satisfied(self) -> bool:
+        for chunk, node in self.collective.postcondition:
+            if self.chunk_arrival_at_node[node][chunk]>self.current_time:
+                return False
+        return True
+
+    def get_available_links(self) -> List[LinkId]:
+        return [edge for edge,busy_until in self.link_busy_until.items() if busy_until<=self.current_time]
+
+    def get_chunks_at_node(self, node: NpuId) -> List[ChunkId]:
+        return [chunk for chunk, arrival_time in self.chunk_arrival_at_node[node].items() if arrival_time<=self.current_time]
+
+    def is_productive_link_chunk_match(self, edge: LinkId, chunk: ChunkId) -> bool:
+        src, dest = edge
+        return (
+            self.link_busy_until[edge]<=self.current_time and # available
+            self.chunk_arrival_at_node[src][chunk]<=self.current_time and # chunk is available at source
+            self.chunk_arrival_at_node[dest][chunk]==float('inf') and # dest does not have it AND not enroute
+            (chunk, dest) in self.collective.postcondition # chunk is needed at dest
+        )
+
+    def get_possible_link_chunk_matches(self) -> List[Tuple[LinkId,ChunkId]]:
+        matches = []
+        available_links = self.get_available_links()
+        for src, dest in available_links:
+            for chunk in self.get_chunks_at_node(src):
+                if self.is_productive_link_chunk_match(edge=(src, dest), chunk=chunk):
+                    matches.append(((src, dest), chunk))
+        return matches
+
+    def match(self, edge: LinkId, chunk: ChunkId) -> None:
+        src, dest = edge
+        if not self.is_productive_link_chunk_match(edge=edge, chunk=chunk):
+            raise ValueError(f"Attempted invalid link chunk match: {edge}, {chunk}")
+        send_time = self.current_time
+        receive_time = self.current_time + self.topology.get_delay(edge=edge, chunk_size=self.chunk_size)
+        self.event_history.append((edge,chunk,send_time,receive_time))
+        self.link_busy_until[edge] = receive_time
+        self.chunk_arrival_at_node[dest][chunk] = receive_time
+        self.event_queue.push((edge,chunk,send_time,receive_time))
+
+    def step(self) -> None:
+        next_time, events = self.event_queue.pop()
+        # for edge,chunk,send_time,receive_time in events:
+        self.current_time = next_time
+
+    def discretize(self) -> None:
+        pass
+
+    def write_ten(self, filename: str) -> None:
+        pass
     
     def solve(self, time_limit: float = None, verbose: bool = False, filename: str = None) -> None:
-        while not self.TEN.satisfied():
-            possible_matches = self.TEN.get_possible_link_chunk_matches()
+        while not self.satisfied():
+            possible_matches = self.get_possible_link_chunk_matches()
             if len(possible_matches)==0:
-                self.TEN.step()
+                self.step()
             else:
                 chosen_edge, chosen_chunk = random.choice(possible_matches)
-                self.TEN.match(edge=chosen_edge, chunk=chosen_chunk)
+                self.match(edge=chosen_edge, chunk=chosen_chunk)
     
     def write_csv(self, filename: str) -> None:
         edge_to_chunks = defaultdict(list)
-        for edge,chunk,send_time,receive_time in self.TEN.event_history:
+        for edge,chunk,send_time,receive_time in self.event_history:
             edge_to_chunks[edge].append((chunk, send_time, receive_time))
 
         with open(filename, mode="w") as f:
@@ -41,7 +100,7 @@ class NaiveSynthesizer:
             writer.writerow(["Links Count",len(self.edges)])
             writer.writerow(["Chunks Count",len(self.chunks)])
             writer.writerow(["Chunk Size",self.chunk_size])
-            writer.writerow(["Collective Time",self.TEN.current_time,"ns"])
+            writer.writerow(["Collective Time",self.current_time,"ns"])
             writer.writerow(["SrcID","DestID","Latency (ns)","Bandwidth (GB/s)","Chunks (ID:ns:ns)"])
             for edge in self.edges:
                 src, dest = edge
